f42e9029df47fc72c2c3ccff34a573ceb98d81fc
Delvric Tezeno <mr.tezeno@gmail.com>
2019-09-12 21:51:02 -0500

    correct fizzbuzz

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function fizzbuzz (maxvalue){ for (let i = 0; i <=  maxvalue; i++){
-   if (i % 2 === 0){
-       console.log("fizz") 
+const numbers = []
+function fizzBuzz (maxValue){ 
+    for (let i = 1; i <=  maxValue; i++){
+   if ( i % 3 === 0 && i %  2 === 0 ){
+    numbers.push ("FizzBuzz, ")
    }
-   else if (i % 3 === 0){
-        console.log ("buzz")
+   else if (i % 2 === 0){
+       numbers.push ("Fizz, ")
 
    }
-   else if ( i % 2 === 0 && i % 3 === 0 ){
-       console.log ("fizzbuzz")
+   else if (i % 3 === 0){
+    numbers.push ("Buzz, ") 
    }
-   else {console.log (i)}
+   else if(! i % 2 === 0 || !i % 3 === 0){ 
+   numbers.push (i+ ", ")
+} 
 }
+ return numbers;
+
 }
-console.log (fizzbuzz(100))
+
+console.log (fizzBuzz(100))
